ef31d4fbac53faa916e6c8b2338ae3828e98d958
신희성 <vvwv3092@gmail.com>
2024-11-13 14:13:28 +0900

    add: User Temp Domain

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM ubuntu:latest
+LABEL authors="huiseongshin"
+ENTRYPOINT ["top", "-b"]
---
+++ src/main/kotlin/com/example/gsmkotlin/domain/user/type/Authority.kt
@@ -0,0 +1,5 @@
+package com.example.gsmkotlin.domain.user.type
+
+enum class Authority {
+    STUDENT, TEACHER, ADMIN
+}
