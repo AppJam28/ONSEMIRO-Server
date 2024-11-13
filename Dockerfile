b36678054c903a3453dcbd3ac80e9084b98a755e
신희성 <vvwv3092@gmail.com>
2024-11-13 15:40:53 +0900

    add: error setting

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,6 @@
-FROM ubuntu:latest
-LABEL authors="huiseongshin"
-ENTRYPOINT ["top", "-b"]
+FROM openjdk:11-jdk-slim
+WORKDIR /app
+ARG JAR_FILE=/build/libs/*.jar
+COPY ${JAR_FILE} app.jar
+ENV TZ=Asia/Seoul
+CMD ["java", "-jar", "-Dspring.profiles.active=prod", "app.jar"]
---
+++ src/main/kotlin/com/example/gsmkotlin/global/error/GlobalException.kt
@@ -0,0 +1,10 @@
+package com.example.gsmkotlin.global.error
+
+import org.springframework.http.HttpStatus
+
+class GlobalException (
+    override val message: String,
+    val httpStatus: HttpStatus
+) : RuntimeException(message) {
+    constructor(httpStatus: HttpStatus) : this(httpStatus.reasonPhrase, httpStatus)
+}
