2c21df3faad4d857477fc6fde75b6f038785bbf5
신희성 <vvwv3092@gmail.com>
2024-11-13 14:02:42 +0900

    settings gsm spring template

---
+++ src/main/kotlin/com/example/gsmkotlin/domain/user/type/User.kt
@@ -0,0 +1,4 @@
+package com.example.gsmkotlin.domain.user.type
+
+class User {
+}
---
+++ src/test/kotlin/com/example/gsmkotlin/GsmKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.gsmkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GsmKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
